a89ff8fd44511f70ae495cae8054b5630cb7dcce
panos1962 <panos1962@gmail.com>
2022-03-31 05:24:34 +0300

    modifications

--- client/exodos/index.php
+++ client/exodos/index.php
@@ -8,7 +8,9 @@ unset($_SESSION["klidi"]);
 </head>
 <body>
 <meta http-equiv="refresh" content="0; URL=<?php
-print (Globals::perastike("url") ? $_REQUEST["url"] : "http://prefadoros.gr");
+print ((isset($_REQUEST) && is_array($_REQUEST) &&
+	array_key_exists("url", $_REQUEST)) ?
+	$_REQUEST["url"] : "http://prefadoros.gr");
 ?>" />
 </body>
 </html>
